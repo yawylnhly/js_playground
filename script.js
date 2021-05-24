97f15c821d9f11d8f866607cb02a6cdcdd8e5d13
yawylnhly <yawylnhly@gmail.com>
2021-05-24 19:50:06 +0300

    exc1

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(23+97);
+console.log(1+2+3+4+5+6);
+console.log((4+6+9)/77);
+
+let a=10;
+console.log(a);
+9*a;
+let b=7 * a;
+
+const max = 57;
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
